6deef88f8f9aac3e7d577e0d3dbabbe962050ce9
TARUNIKA CHAUHAN <99027009+TarunikaChauhan@users.noreply.github.com>
2023-07-28 22:13:23 +0530

    Update health_monitoring_system.py

--- health_monitoring_system.py
+++ health_monitoring_system.py
@@ -1,45 +1,96 @@
-import requests
-import time
-import random
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME680.h>
+#include <SoftwareSerial.h>
 
-# Replace with the server URL or IP address to send data
-server_url = "http://your_server_ip_or_domain/health_data"
+// Replace with your Wi-Fi credentials
+const char* ssid = "your_SSID";
+const char* password = "your_PASSWORD";
 
-def generate_health_data():
-    # Simulate generating health data (blood pressure, sugar level, and heartbeat)
-    # Replace this with actual code to read data from sensors in your hardware setup
-    blood_pressure = random.randint(100, 140)  # Simulate systolic blood pressure (mmHg)
-    sugar_level = random.uniform(80, 150)  # Simulate blood sugar level (mg/dL)
-    heartbeat = random.randint(60, 100)  # Simulate heart rate (bpm)
-    return blood_pressure, sugar_level, heartbeat
+// Replace with your server URL or IP address
+const char* serverURL = "http://your_server_ip_or_domain/health_data.php";
 
-def send_data_to_server(data):
-    # Send health data to the server using HTTP POST request
-    try:
-        response = requests.post(server_url, json=data)
-        if response.status_code == 200:
-            print("Data sent successfully to the server!")
-        else:
-            print("Failed to send data. Error code:", response.status_code)
-    except requests.exceptions.RequestException as e:
-        print("Failed to send data:", e)
+const int heartRateSensorPin = A0;
+const int bloodPressureSensorPin = A1;
+const int sugarLevelSensorPin = A2;
 
-def main():
-    while True:
-        # Generate simulated health data
-        blood_pressure, sugar_level, heartbeat = generate_health_data()
-        
-        # Create a dictionary with the health data
-        data_to_send = {
-            "blood_pressure": blood_pressure,
-            "sugar_level": sugar_level,
-            "heartbeat": heartbeat,
-        }
+Adafruit_BME680 bme;  // Initialize the BME680 sensor
 
-        # Send data to the server
-        send_data_to_server(data_to_send)
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  bme.begin(0x76);  // Start the BME680 sensor
 
-        time.sleep(60)  # Send data every 60 seconds
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
 
-if __name__ == "__main__":
-    main()
+  Serial.println("Connected to WiFi");
+}
+
+void loop() {
+  // Read sensor data
+  float heartRate = readHeartRate();
+  float bloodPressure = readBloodPressure();
+  float sugarLevel = readSugarLevel();
+  float temperature = readTemperature();
+  float humidity = readHumidity();
+
+  // Send the data to the server
+  sendHealthData(heartRate, bloodPressure, sugarLevel, temperature, humidity);
+
+  delay(60000);  // Send data every minute
+}
+
+float readHeartRate() {
+  // Read data from the heart rate sensor and return the heart rate value
+  // You need to implement the specific logic for your heart rate sensor
+}
+
+float readBloodPressure() {
+  // Read data from the blood pressure sensor and return the blood pressure value
+  // You need to implement the specific logic for your blood pressure sensor
+}
+
+float readSugarLevel() {
+  // Read data from the sugar level sensor and return the sugar level value
+  // You need to implement the specific logic for your sugar level sensor
+}
+
+float readTemperature() {
+  // Read data from the BME680 sensor and return the temperature value
+  return bme.readTemperature();
+}
+
+float readHumidity() {
+  // Read data from the BME680 sensor and return the humidity value
+  return bme.readHumidity();
+}
+
+void sendHealthData(float heartRate, float bloodPressure, float sugarLevel, float temperature, float humidity) {
+  // Prepare payload for POST request
+  String payload = "heart_rate=" + String(heartRate) +
+                   "&blood_pressure=" + String(bloodPressure) +
+                   "&sugar_level=" + String(sugarLevel) +
+                   "&temperature=" + String(temperature) +
+                   "&humidity=" + String(humidity);
+
+  HTTPClient http;
+  http.begin(serverURL);
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+  int httpResponseCode = http.POST(payload);
+
+  if (httpResponseCode > 0) {
+    String response = http.getString();
+    Serial.println("Server response: " + response);
+  } else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+
+  http.end();
+}
